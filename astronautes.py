16a164c7013cbf7f26985951358339e50996413d
VEILLEClemence <veilleclemence@orange.fr>
2022-03-11 10:39:09 +0100

    affiche que les astronautes pour lesquels l’attribut craft vaut ISS

---
+++ astronautes.py
@@ -0,0 +1,7 @@
+import requests
+
+contenu = requests.get("http://api.open-notify.org/astros.json")
+for personne in contenu.json()["people"]:
+    if personne["craft"]=='ISS':
+        print(personne)
+
